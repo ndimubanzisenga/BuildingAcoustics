3d70b304d3bf737f95350f1b5f3effb8d0f0f2aa
Ndimubanzi <sengan@measX.lan>
2018-03-13 14:05:34 +0100

    Refactor Dasylab interface and use external library files

--- src/scripts/test/dasylab_test.py
+++ src/scripts/test/dasylab_test.py
@@ -4,82 +4,10 @@ import lys
 import sys
 
 import numpy as np
-from scipy.fftpack import fft, ifft
-from scipy.signal import fftconvolve, convolve
-import math
 
-class RoomResponseEstimator(object):
-    """
-    Gives probe impulse, gets response and calculate impulse response.
-    Method from paper: "Simultaneous measurement of impulse response and distortion with a swept-sine technique"
-    Angelo Farina
-    """
-
-    def __init__(self, duration=10.0, low=100.0, high=15000.0, Fs=44100.0):
-        self.Fs = Fs
-
-        # Total length in samples
-        self.T = Fs*duration
-        self.w1 = low / self.Fs * 2*np.pi
-        self.w2 = high / self.Fs * 2*np.pi
-
-        self.probe_pulse = self.probe()
-        #self.probe_pulse = np.random.random_sample(self.T)
-
-        # Apply exponential signal on the beginning and the end of the probe signal.
-        exp_window = 1-np.exp(np.linspace(0,-10, 5000))
-        self.probe_pulse[:exp_window.shape[0]] *= exp_window
-        self.probe_pulse[-exp_window.shape[0]:] *= exp_window[-1::-1]
-
-        # This is what the value of K will be at the end (in dB):
-        kend = 10**((-6*np.log2(self.w2/self.w1))/20)
-        # dB to rational number.
-        k = np.log(kend)/self.T
-
-        # Making reverse probe impulse so that convolution will just
-        # calculate dot product. Weighting it with exponent to acheive
-        # 6 dB per octave amplitude decrease.
-        self.reverse_pulse = self.probe_pulse[-1::-1] * \
-            np.array(list(\
-                map(lambda t: np.exp(float(t)*k), range(int(self.T)))\
-                )\
-            )
-
-        # Now we have to normilze energy of result of dot product.
-        # This is "naive" method but it just works.
-        corr = fftconvolve(self.reverse_pulse, self.probe_pulse)
-        Frp =  fft(corr)
-        self.reverse_pulse /= np.abs(Frp[round(Frp.shape[0]/4)])
-
-    def probe(self):
-
-        w1 = self.w1
-        w2 = self.w2
-        T = self.T
-
-        # page 5
-        def lin_freq(t):
-            return w1*t + (w2-w1)/T * t*t / 2
-
-        # page 6
-        def log_freq(t):
-            K = T * w1 / np.log(w2/w1)
-            L = T / np.log(w2/w1)
-            return K * (np.exp(t/L)-1.0)
-
-        freqs = log_freq(range(int(T)))
-        impulse = np.sin(freqs)
-        return impulse
-
-    def estimate(self, response):
-
-        I = fftconvolve( response, self.reverse_pulse, mode='full')
-        I = I[self.probe_pulse.shape[0]:self.probe_pulse.shape[0]*2+1]
-
-        peek_x = np.argmax( I, axis=0 )
-        I = I[peek_x:]
-
-        return I
+sys.path.append('C:/Users/sengan/Documents/Projects/BuildingAcoustics/src/scripts')
+from acoustics.building_acoustics_measurement import BuildingAcousticsMeasurement
+from acoustics.generator import Generator
 
 # Module variables initialization
 class info(object):
@@ -87,38 +15,35 @@ class info(object):
         # Variables for settings
         # Click on the help button to get more information.
         # Example variables
-        self.m_a = 1.0
-        self.c_b = [0.1, 1.1, 2.1, 3.1, 4.1, 5.1, 6.1, 7.1, 8.1, 9.1, 10.1, 11.1, 12.1, 13.1, 14.1, 15.1]
-        self.c_c = [0.0 for n in range(16)]
+
+        timebase_id = Ly.GetTimeBaseIDByName('Driver')
+        sample_distance = Ly.GetTimeBaseSampleDistance(timebase_id)
+        sampling_frequency = 1.0 /  float(sample_distance)
+
+        self.sampling_frequency = sampling_frequency
+        self.low_octave_band = 100.
+        self.high_octave_band = 3150.
+        self.fraction = 3
+        self.probe_signal_duration = 10.
+        self.probe_signal_freq_l = 100.
+        self.probe_signal_freq_h = 15000.
 
 # Temporary variables, not saved with the worksheet
 class pvar(object):
-    def __init__(self):
+    def __init__(self, sampling_frequency, probe_signal_duration, probe_signal_freq_l=100., probe_signal_freq_h=15000., low_octave_band=100., high_octave_band=3150., fraction=3):
         # Working variables
         # Click on the help button to get more information.
         # Example variables
-        duration = 10.0
-        lowfreq = 100.0
-        highfreq = 15000.0
-        timebase_id = Ly.GetTimeBaseIDByName('Driver')
-        auto_blocksize = False
-        freq_format = 2
-        var_blocksize = 0
-        var_sample_rate = 0
+        self.generator = Generator(fs=sampling_frequency, duration=probe_signal_duration)
+        noise_type = 'sine_sweep'
 
-        sample_distance = Ly.GetTimeBaseSampleDistance(timebase_id)
-        sampling_frequency = 1.0 /  float(sample_distance)
-        block_size = sampling_frequency
-        print("Sampling Freq : {0}, Block Size : {1}").format(sampling_frequency, block_size)
-
-        #Ly.UpdateTimeBase(timebase_id, auto_blocksize, block_size, freq_format, sample_distance, var_blocksize, var_sample_rate)
-        self.response = None
+        if noise_type == 'sine_sweep':
+            self.probe_signal, self.reverse_signal = self.generator.noise(noise_type)
+        else:
+            self.probe_signal = self.generator.noise(noise_type)
         self.room_response = None
-        self.m_m = 0.0
-        self.c_n = [1.0 for n in range(16)]
-
-        self.estimator = RoomResponseEstimator(duration, lowfreq, highfreq, sampling_frequency)
-        self.blockCount = 0
+        self.building_acoustics_measurement = BuildingAcousticsMeasurement(fs= sampling_frequency, f_start=low_octave_band, f_stop=high_octave_band, fraction=fraction)
+        self.block_count = 0
 
 class pscript(lys.mclass):
     def __init__(self, magic):
@@ -127,10 +52,11 @@ class pscript(lys.mclass):
         #blockSize = 10000
         #sampleDistance = 1 / float(blockSize * 5)
         #Ly.SetTimeBase(timebase_id, blocksize, sampleDistance)
-
-        self.info=info()
-        self.pvar=pvar()
-        print("Initializing module....")
+        print("## Initializing module.... ##")
+        self.info = info()
+        self.pvar = pvar(self.info.sampling_frequency, self.info.probe_signal_duration, self.info.probe_signal_freq_l, self.info.probe_signal_freq_h,\
+                        self.info.low_octave_band, self.info.high_octave_band, self.info.fraction)
+        print("## Initialized module.... ##")
 
     def Create (self):
         # Module initialization
@@ -148,7 +74,7 @@ class pscript(lys.mclass):
         # Click on the help button to get more information.
 
         # Set dialog title
-        dlg.title = "Script (Processing Data)"
+        dlg.title = "Script (Bulding Acoustic Meas.)"
         # Determine the number of channels, current channel and
         # maximum number of channels
         # (Covers 1:1 channel relation with at least one input.
@@ -159,20 +85,28 @@ class pscript(lys.mclass):
         # Setup dialog parameters
         dm = lys.DialogManager(self, dlg)
         dm.SelectModulePage()
-        dm.AppendFloat("Param a", self.info.m_a, "Example: variable m_a in module scope")
-        dm.SelectChannelPage()
-        dm.AppendFloat("Param b", self.info.c_b, "Example: variable c_b in channel scope")
-        dm.AppendFloat("Param c", self.info.c_c, "Example: variable c_c in channel scope")
+        dm.AppendFloat("Param sampling_frequency", self.info.sampling_frequency, "Sampling frequency with which the signal is to be generated and measured")
+        dm.AppendFloat("Param low_octave_band", self.info.low_octave_band, "Lowest octave band center frequency")
+        dm.AppendFloat("Param high_octave_band", self.info.high_octave_band, "Highest octave band center frequency")
+        dm.AppendFloat("Param fraction", self.info.fraction, "Octave band fraction")
+        dm.AppendFloat("Param probe_signal_duration", self.info.probe_signal_duration, "Duration of the probe signal")
+        dm.AppendFloat("Param probe_signal_freq_l", self.info.probe_signal_freq_l, "Lowest frequency of the probe signal")
+        dm.AppendFloat("Param probe_signal_freq_h", self.info.probe_signal_freq_h, "Highest frequency of the probe signal")
 
     def DlgOk (self, dlg):
         # Get values of dialog parameters
         # Click on the help button to get more information.
         dom = lys.DialogOkManager(dlg)
         dom.SelectModulePage()
-        self.info.m_a = dom.GetValue("Param a")
-        dom.SelectChannelPage()
-        self.info.c_b = dom.GetValue("Param b")
-        self.info.c_c = dom.GetValue("Param c")
+        self.info.low_octave_band = dom.GetValue("Param low_octave_band")
+        self.info.high_octave_band = dom.GetValue("Param high_octave_band")
+        self.info.fraction = dom.GetValue("Param fraction")
+        self.info.probe_signal_duration = dom.GetValue("Param probe_signal_duration")
+        self.info.probe_signal_freq_l = dom.GetValue("Param probe_signal_freq_l")
+        self.info.probe_signal_freq_h = dom.GetValue("Param probe_signal_freq_h")
+
+        self.pvar = pvar(self.info.sampling_frequency, self.info.probe_signal_duration, self.info.probe_signal_freq_l, self.info.probe_signal_freq_h,\
+                        self.info.low_octave_band, self.info.high_octave_band, self.info.fraction)
 
         # Configure Inputs and Outputs
         # (Covers 1:1 channel relation with at least one input.
@@ -199,7 +133,7 @@ class pscript(lys.mclass):
         # Initialize variables on start of measurement (if needed)
         # Click on the help button to get more information.
         print("Reinitializing module....")
-        self.pvar.blockCount = 0
+        self.pvar.block_count = 0
 
         return True
 
@@ -220,7 +154,7 @@ class pscript(lys.mclass):
         # need adjustments in this case!
 
         # r = math.exp((v+c) / N)
-        r = self.pvar.estimator.probe_pulse[c + (N * self.pvar.blockCount)]
+        r = self.pvar.estimator.probe_pulse[c + (N * self.pvar.block_count)]
         return r
 
     def ProcessData (self):
@@ -229,16 +163,14 @@ class pscript(lys.mclass):
         # if you have chosen a user defined setting because you very likely
         # need adjustments in this case!
 
-        def numpyToDasylab(outChannel, inBuffer, numpyDataArray):
-            outBuffer = self.GetOutputBlock(outChannel)
-            blockSize = inBuffer.BlockSize
-            dataLength = numpyDataArray.shape[0]
+        def numpyToDasylab(outBuffer, numpyDataArray):
+            blockSize = outBuffer.BlockSize
+            dataLength = numpyDataArray.size
 
             if (outBuffer == None):
                 self.ShowWarning("Could not get output block! Stopped!")
                 Ly.StopExperiment()
                 return
-
             if (blockSize < dataLength):
                 numpyDataArray = numpyDataArray[:blockSize]
 
@@ -247,72 +179,85 @@ class pscript(lys.mclass):
 
             for i in xrange(numpyDataArray.shape[0]):
                 outBuffer[i] = numpyDataArray[i]
-
-            outBuffer.StartTime = inBuffer.StartTime
-            outBuffer.SampleDistance = inBuffer.SampleDistance
-            outBuffer.BlockSize = inBuffer.BlockSize
-            outBuffer.Release()
-
             return
-
 
         def dasylabToNumpy(inBuffer):
             data =  list()
-
             for i in xrange(inBuffer.BlockSize):
                 data.append(inBuffer[i])
-
             return (np.asarray(data))
-
 
         # All inputs available?
         for i in range(self.NumInChannel):
             if (self.GetInputBlock(i) == None):
                 return True
 
-        # Process data
-        #for channel in xrange(self.NumInChannel):
-        channel0 = 0
-        channel1 = 1
-        inBuffer = self.GetInputBlock(channel0)
-        blockSize = inBuffer.BlockSize
-        sequenceLength = self.pvar.estimator.probe_pulse.shape[0]
-        blocksNumber = sequenceLength / blockSize
+        # Get input blocks
+        channel_data = 0
+        channel_selector = 1
+        acquired_data_buffer = self.GetInputBlock(channel_data)
+        room_selector_buffer = self.GetInputBlock(channel_selector)
 
-        if (blockSize * self.pvar.blockCount) < sequenceLength:
-            #print("Block : {0}  of  :  {1} . Sequence length : {2} . Input block size  : {3}. Input sampling freq : {4}").format(self.pvar.blockCount, blocksNumber, sequenceLength, blockSize, (1/inBuffer.SampleDistance))
-            inBlockData = dasylabToNumpy(inBuffer)
-            outBlockData = None
+        block_size = acquired_data_buffer.BlockSize
+        sequence_length = self.pvar.probe_signal.size
 
-            # Output generated sequence
-            if (sequenceLength - blockSize * self.pvar.blockCount) < blockSize:
-                # Case where the generated sequence length is not a multiple of the block size
-                outBlockData = self.pvar.estimator.probe_pulse[(blockSize * self.pvar.blockCount):]
-                inBlockData = inBlockData[:outBlockData.shape[0]]
+        is_data_acquisition_done = Ly.GetVar(1)
+        if not is_data_acquisition_done:
+            # Process data
+            if (block_size * self.pvar.block_count) < sequence_length:
+                acquired_data_block = dasylabToNumpy(acquired_data_buffer)
+                output_data_block = None
+
+                if (sequence_length - block_size * self.pvar.block_count) < block_size:
+                    # Check if this is the last block of the generated signal
+                    # Check if the last block of the generated signal has fewer samples than the block_size
+                    output_data_block = self.pvar.probe_signal[(block_size * self.pvar.block_count):]
+                    acquired_data_block = acquired_data_block[:output_data_block.size]
+                else:
+                    # Case where the generated signal block is a equal to the block_size
+                    output_data_block = self.pvar.probe_signal[(block_size * self.pvar.block_count):(block_size * (self.pvar.block_count+1))]
+
+                probe_signal_out_buffer = self.GetOutputBlock(channel_data)
+                probe_signal_out_buffer.BlockSize = acquired_data_buffer.BlockSize
+                probe_signal_out_buffer.StartTime = acquired_data_buffer.StartTime
+                probe_signal_out_buffer.SampleDistance = acquired_data_buffer.SampleDistance
+
+                numpyToDasylab(probe_signal_out_buffer, output_data_block)
+
+                probe_signal_out_buffer.Release()
+                # Store acquired signal
+                if (self.pvar.block_count is 0):
+                    self.pvar.room_response = acquired_data_block
+                else:
+                    self.pvar.room_response = np.append(self.pvar.room_response, acquired_data_block)
+                self.pvar.block_count = self.pvar.block_count + 1
             else:
-                # Case where the generated sequence length is a multiple od the block size
-                outBlockData = self.pvar.estimator.probe_pulse[(blockSize * self.pvar.blockCount):(blockSize * (self.pvar.blockCount+1))]
+                print("Probe Signal : {0}").format(self.pvar.probe_signal.shape)
+                print("Room response : {0}").format(self.pvar.room_response.shape)
 
-            numpyToDasylab(channel0, inBuffer, outBlockData)
 
-            # Store acquired signal
-            if (self.pvar.blockCount is 0):
-                self.pvar.response = inBlockData
-            else:
-                self.pvar.response = np.append(self.pvar.response, inBlockData)
+                impulse_response = self.pvar.generator.estimate_impulse_response(self.pvar.room_response[0:], self.pvar.reverse_signal)
+                #np.savetxt('C:/Users/sengan/Documents/Projects/BuildingAcoustics/data/impulse_response.log', impulse_response)
+                print ("Impulse response : {0}").format(impulse_response.shape)
+                if room_selector_buffer[0] > 0:
+                    # Transmitting room selected
+                    print("Transmitting room selected")
+                    self.pvar.building_acoustics_measurement.compute_spl('tx', self.pvar.room_response)
+                    self.pvar.building_acoustics_measurement.compute_reverberation_time('tx', impulse_response)
+                elif room_selector_buffer[0] < 0:
+                    # Receiving room selected
+                    print("Receiving room selected")
+                    self.pvar.building_acoustics_measurement.compute_spl('rx', self.pvar.room_response)
+                    self.pvar.building_acoustics_measurement.compute_reverberation_time('rx', impulse_response)
+                else:
+                    # No room selected
+                    print("No room selected")
+                Ly.SetVar(1, 5.0) # Terminate data acquisition
+                self.pvar.block_count = 0 # Reset probe signal output block count
+                print("Reverberation time:\n{0}").format(self.pvar.building_acoustics_measurement.reverberation_time)
 
-        else:
-            print("Computing room response....")
-            inBuffer = self.GetInputBlock(channel1)
-            self.room_response = self.pvar.estimator.estimate(self.pvar.response)
-            np.savetxt('C:/Users/sengan/Documents/Test/src/room_response.log', self.room_response)
-            numpyToDasylab(channel1, inBuffer, self.room_response[:blockSize])
-            print("Room response length : {0}").format(self.room_response.shape)
-            Ly.StopExperiment()
-            return True
-
-        print ("Block : {0} . Sequence length : {1} . Response length : {2}").format(self.pvar.blockCount, self.pvar.estimator.probe_pulse.shape, self.pvar.response.shape)
-        self.pvar.blockCount = self.pvar.blockCount + 1
-        inBuffer.Release()
-
+            #output_data = np.zeros(acquired_data_buffer.BlockSize)
+            #numpyToDasylab(0, acquired_data_buffer, output_data)
+        acquired_data_buffer.Release()
+        room_selector_buffer.Release()
         return True
