b633e16fa49af388c82b518c445c49d069770a84
Ndimubanzi <sengan@measX.lan>
2018-07-11 21:19:12 +0200

    considers only frequencies up to 3150 dor evaluation

--- src/scripts/acoustics/building_acoustics_measurement.py
+++ src/scripts/acoustics/building_acoustics_measurement.py
@@ -367,6 +367,7 @@ class BuildingAcousticsMeasurement(object):
             t60[i] = factor * (db_regress_end - db_regress_init)
 
             if i == test_octave_band:
+                print "Test RT60 : ",t60[i]
                 y_ = m * x + b
                 self.bandpass_filtered_ir = filtered_signal
                 self.schroeder_curve = sch
@@ -375,4 +376,4 @@ class BuildingAcousticsMeasurement(object):
         return t60
 
     def get_ref_curve(self):
-        return np.array([[0, 3, 6, 9, 12, 15, 18, 19, 20, 21, 22, 23, 23, 23, 23, 23, 23, 23]])
+        return np.array([[0, 3, 6, 9, 12, 15, 18, 19, 20, 21, 22, 23, 23, 23, 23, 23]])
